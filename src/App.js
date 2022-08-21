bdf10e1cb08d7bc1cf458127f47c727252e575da
unknown <kh0shmaze@gmail.com>
2022-08-21 20:55:41 +0430

    simple final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./component/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/Welcome">
-          <Welcome />
-        </Route>
-        <Route path="/Products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1"> ABook</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p31">An Online Course</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
